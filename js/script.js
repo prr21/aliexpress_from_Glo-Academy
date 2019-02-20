79825d51fdc3bf375365f536248f0d539ced0c87
prr <Mr.gustav009@gmail.com>
2019-02-20 21:02:53 +0300

    Загрузка json

--- js/script.js
+++ js/script.js
@@ -1,117 +1,158 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    const   cartWrapper = document.querySelector('.cart__wrapper'),
-            cart = document.querySelector('.cart'),
-            close = document.querySelector('.cart__close'),
-            open = document.querySelector('#cart'),
-            goodsBtn = document.querySelectorAll('.goods__btn'),
-            products = document.querySelectorAll('.goods__item'),
-            confirm = document.querySelector('.confirm'),
-            badge = document.querySelector('.nav__badge'),
-            totalCost = document.querySelector('.cart__total > span'),
-            empty = cartWrapper.querySelector('.empty'),
-            titles = document.querySelectorAll('.goods__title');
-
-    // Появление корзины
-    open.addEventListener('click', openCart);
-
-    function openCart(){
-        cart.classList.add('show')
-        document.body.style.overflow = 'hidden';
-    }
-
-    // Закрытие корзины
-    close.addEventListener('click', closeCart);
-
-    function closeCart(){
-        cart.classList.remove('show')
-        document.body.style.overflow = 'auto';
-    }
-
-    // Добавление продукта в корзину
-    goodsBtn.forEach((btn, i) => {
-        btn.addEventListener('click', () => {
-
-            let item = products[i].cloneNode(true),
-                trigger = item.querySelector('button'),
-                removeBtn = document.createElement('div');
-            
-            trigger.remove();
-
-            showConfirm(); //Анимация корзины
-            countBadge(1); //Увеличить счётчик корзины
-
-            removeBtn.classList.add('goods__item-remove');
-            removeBtn.innerHTML = '&times';
-            item.appendChild(removeBtn);
-
-            cartWrapper.appendChild(item);
-            
-            calcPrice() //Подсчитать сумму товаров
-            removeFromCart() //Объявить об обработчике событий
-
-            if(empty) empty.style.display = 'none';
-        })    
-    });
-
-    // Сокращение описания товара
-    titles.forEach((item) => {
-        if (item.textContent.length > 70) {
-            item.textContent = item.textContent.slice(0,70) + '...';
-        } 
-    })
+    const loadContent = async (url, callback) => {
+        await fetch(url)
+            .then(response => response.json())
+            .then(json => createElement(json.goods));
     
-    // Анимация появления корзины
-    function showConfirm(){
-        let count = 100;
-
-        var intervalId = setInterval(() => {
-            if(count != 10){
-                confirm.style.cssText = 
-                    `display: block;
-                    transform: translateY(-${count}px);
-                    opacity: .${count}`
-                count-=2;
-
-            } else {
-                confirm.style.display = 'none';
-                clearInterval(intervalId);
-            }
-        }, 15);
+        callback();
     }
 
-    // Смена кол-во товаров на счетчике корзины
-    function countBadge(i){
-        let items = cartWrapper.querySelectorAll('.goods__item');
-        badge.textContent = items.length + i ;
-    }
+    // Создание товара
+    function createElement(arr) {
+        const goodsWrapper = document.querySelector('.goods__wrapper');
     
-    // Подсчёт итоговой стоимости
-    function calcPrice(){
-        let price = cartWrapper.querySelectorAll('.goods__price > span'),
-            total = 0;
-
-        price.forEach((item) => {
-            total += +item.textContent;
-        })
-        totalCost.textContent = total;
+        arr.forEach(function(item) {
+            let card = document.createElement('div');
+            card.classList.add('goods__item');
+            card.innerHTML = `    
+                <img class="goods__img" src="${item.url}" alt="phone">
+                <div class="goods__colors">Доступно цветов: 4</div>
+                <div class="goods__title">
+                    ${item.title} 
+                </div>
+                <div class="goods__price">
+                    <span>${item.price}</span> руб/шт
+                </div>
+                <button class="goods__btn">Добавить в корзину</button>
+            `;
+            goodsWrapper.appendChild(card);
+        });
     }
 
-    // Удаление товара из корзины
-    function removeFromCart(){
-        let removeBtn = cartWrapper.querySelectorAll('.goods__item-remove');
+    loadContent('js/db.json', () => {
+        const   cartWrapper = document.querySelector('.cart__wrapper'),
+                cart = document.querySelector('.cart'),
+                close = document.querySelector('.cart__close'),
+                open = document.querySelector('#cart'),
+                goodsBtn = document.querySelectorAll('.goods__btn'),
+                products = document.querySelectorAll('.goods__item'),
+                confirm = document.querySelector('.confirm'),
+                badge = document.querySelector('.nav__badge'),
+                totalCost = document.querySelector('.cart__total > span'),
+                empty = cartWrapper.querySelector('.empty'),
+                titles = document.querySelectorAll('.goods__title');
 
-        removeBtn.forEach((btn) => {
-            btn.addEventListener('click', function() {
-                btn.parentElement.remove();
+        // Появление корзины
+        open.addEventListener('click', openCart);
 
-                countBadge(0); //Уменьшить счётчик корзины
-                calcPrice(); //Подсчитать сумму товаров
+        function openCart(){
+            cart.classList.add('show')
+            document.body.style.overflow = 'hidden';
+        }
+
+        // Закрытие корзины
+        close.addEventListener('click', closeCart);
+
+        function closeCart(){
+            cart.classList.remove('show')
+            document.body.style.overflow = 'auto';
+        }
+
+        // Добавление продукта в корзину
+        goodsBtn.forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+
+                let item = products[i].cloneNode(true),
+                    trigger = item.querySelector('button'),
+                    removeBtn = document.createElement('div');
                 
-                if(!cartWrapper.querySelectorAll('.goods__item').length) empty.style.display = '';
-            })
+                trigger.remove();
+
+                showConfirm(); //Анимация корзины
+                countBadge(1); //Увеличить счётчик корзины
+
+                removeBtn.classList.add('goods__item-remove');
+                removeBtn.innerHTML = '&times';
+                item.appendChild(removeBtn);
+
+                cartWrapper.appendChild(item);
+                
+                calcPrice() //Подсчитать сумму товаров
+                removeFromCart() //Объявить об обработчике событий
+
+                if(empty) empty.style.display = 'none';
+            })    
+        });
+
+        // Сокращение описания товара
+        titles.forEach((item) => {
+            if (item.textContent.length > 70) {
+                item.textContent = item.textContent.slice(0,70) + '...';
+            } 
         })
-    }
+        
+        // Анимация появления корзины
+        function showConfirm(){
+            let count = 100;
 
-})
+            var intervalId = setInterval(() => {
+                if(count != 10){
+                    confirm.style.cssText = 
+                        `display: block;
+                        transform: translateY(-${count}px);
+                        opacity: .${count}`
+                    count-=2;
 
+                } else {
+                    confirm.style.display = 'none';
+                    clearInterval(intervalId);
+                }
+            }, 15);
+        }
+
+        // Смена кол-во товаров на счетчике корзины
+        function countBadge(i){
+            let items = cartWrapper.querySelectorAll('.goods__item');
+            badge.textContent = items.length + i ;
+        }
+        
+        // Подсчёт итоговой стоимости
+        function calcPrice(){
+            let price = cartWrapper.querySelectorAll('.goods__price > span'),
+                total = 0;
+
+            price.forEach((item) => {
+                total += +item.textContent;
+            })
+            totalCost.textContent = total;
+        }
+
+        // Удаление товара из корзины
+        function removeFromCart(){
+            let removeBtn = cartWrapper.querySelectorAll('.goods__item-remove');
+
+            removeBtn.forEach((btn) => {
+                btn.addEventListener('click', function() {
+                    btn.parentElement.remove();
+
+                    countBadge(0); //Уменьшить счётчик корзины
+                    calcPrice(); //Подсчитать сумму товаров
+                    
+                    if(!cartWrapper.querySelectorAll('.goods__item').length) empty.style.display = '';
+                });
+            });
+        }
+    });
+    
+});
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts', 
+//     {
+//         method: "POST",
+//         body: JSON.stringify(example)
+//     })  //Promice
+// .then(response => response.json())  //Promice
+// .then(json => console.log(json))
